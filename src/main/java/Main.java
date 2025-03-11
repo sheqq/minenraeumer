6b36c13d33cdd5109bba5dda7d36ca890ca91229
sheqq <pscheuber04@gmail.com>
2025-03-11 09:51:14 +0100

    init javafx

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 package main.java;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,8 +14,16 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         System.out.println("start");
-        GenerateMap map = new GenerateMap();
+        try {
+            BorderPane root = new BorderPane();
+            Scene scene = new Scene(root, 400, 400);
+            stage.setScene(scene);
+            stage.show();
 
-        map.printBackend();
+            GenerateMap map = new GenerateMap();
+            map.printBackend();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
